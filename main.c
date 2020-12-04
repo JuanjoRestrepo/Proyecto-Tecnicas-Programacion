f6811b57a6982f39991f87b377691e74e00ed6b5
juanjorestrepo <49797614+JuanjoRestrepo@users.noreply.github.com>
2020-12-04 15:14:45 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,39 @@
 #include"centroComercial.h"
 
+void menuGeneral(){
+	int opcion;
+	
+	do{
+		system("cls");
+		printf("1. Parqueadero\n");
+		printf("2. Centro Comercial\n");
+		printf("0. Terminar Programa\n");
+		printf("Que quieres: ");
+		scanf("%d", &opcion);
+		fflush(stdin);
+		switch(opcion){
+			
+			case 0:
+				break;
+				
+			case 1:
+				
+				crearParqueadero();
+				break;
+				
+			case 2:
+				
+				crearCentroComercial();
+				break;
+				
+			default:
+				printf("\nOpcion no valida. Intente de nuevo\n");
+				system("pause");
+		}
+		
+	}while(opcion != 0);
+}
+
 int main(){
 
 	menuGeneral();
